e674acc59e7d49edb5b36b52ef55571740f26242
Giselle Johnson <165144397+Gise345@users.noreply.github.com>
2024-06-13 02:11:54 +0100

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,9 @@
+/** ===== typing Annimation ===== */
+
+var typed = new Typed(".typing", {
+    strings:["", "Full Stack Engineer", "AWS Certified Professional"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+
+})
